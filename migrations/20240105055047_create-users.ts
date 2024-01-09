000576ec0be41279cdb8ed842c133e36ada82987
Felipe Makoto <felipe.makoto@e-train.com.au>
2024-01-09 16:01:21 +1030

    created meals table

--- migrations/20240105055047_create-users.ts
+++ migrations/20240105055047_create-users.ts
@@ -1,10 +1,13 @@
-import type { Knex } from "knex";
-
+import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.string('name').notNullable()
+    table.string('email').notNullable()
+  })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
 }
-
