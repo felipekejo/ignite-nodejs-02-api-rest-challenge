aa4e27320b7dded7b74b9f8166796e7357e5aee1
Felipe Makoto <felipe.makoto@e-train.com.au>
2024-01-10 15:26:22 +1030

    added zod

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,5 @@
-import 'dotenv/config'
 import { Knex, knex as setupKnex } from 'knex'
+import { env } from './env'
 
 if (!process.env.DATABASE_URL) {
   throw new Error('Please provide a DATABASE_URL environment variable.')
@@ -8,7 +8,7 @@ if (!process.env.DATABASE_URL) {
 export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: process.env.DATABASE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -10,6 +11,6 @@ app.get('/users', async (request, reply) => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => console.log('Server is running!'))
